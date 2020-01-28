a5a5656741b8b098d475c00c54c163825b97e05c
Erik Gúth <guthember@gmail.com>
2020-01-28 15:48:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haromszog
+{
+  class Program
+  {
+    static double Terulet(double a, double b, double c)
+    {
+      double s = (a + b + c) / 2;
+      return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+    
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Terulet(3,4,5));
+
+      Console.ReadKey();
+    }
+  }
+}
